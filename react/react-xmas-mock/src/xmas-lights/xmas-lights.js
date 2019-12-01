02544d61c42dafb1db38b790773e496984fa3dee
Kristjan Koemets <koemets.kristjan@gmail.com>
2019-12-01 14:55:01 +0200

    Ref

--- react/react-xmas-mock/src/xmas-lights/xmas-lights.js
+++ react/react-xmas-mock/src/xmas-lights/xmas-lights.js
@@ -3,7 +3,7 @@ import './xmas-lights.css';
 
 const XmasLights = props => {
     return <ul className="lightrope">
-        [...Array(50).keys()].map(() => <li></li>)}
+        {[...Array(50).keys()].map(() => <li></li>)}
     </ul>
 };
 export default XmasLights;
