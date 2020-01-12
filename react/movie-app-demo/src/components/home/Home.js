85fec6fca7fc666bba6f86a793e0172b0e9ed8d0
Kristjan Koemets <koemets.kristjan@gmail.com>
2020-01-12 16:58:18 +0200

    movie-app-demo: update components

--- react/movie-app-demo/src/components/home/Home.js
+++ react/movie-app-demo/src/components/home/Home.js
@@ -5,7 +5,7 @@ import SearchBar from '../searchBar/SearchBar';
 import FourColGrid from '../fourColGrid/FourColGrid';
 import Spinner from '../spinner/Spinner';
 import LoadMoreBtn from '../loadMoreBtn/LoadMoreBtn';
-import {API_KEY, API_URL} from '../../apiConfig/config';
+import {API_KEY, API_URL, BACKDROP_SIZE, IMAGE_BASE_URL, LANGUAGE} from '../../apiConfig/config';
 
 class Home extends Component {
     state = {
@@ -17,24 +17,60 @@ class Home extends Component {
         searchTerm: ''
     };
 
+
+    loadMoreItems = () => this.setState({loading: true}, () => {
+        const {currentPage, searchTerm} = this.state;
+        const page = currentPage + 1;
+        const endPoint = !searchTerm ?
+            `${API_URL}movie/popular?api_key=${API_KEY}&${LANGUAGE}&page=${page}` :
+            `${API_URL}search/movie?api_key=${API_KEY}&${LANGUAGE}&query${searchTerm}&page=
+            ${page}`;
+        this.fetchItems(endPoint);
+    });
+
+    searchItems = searchTerm => this.setState({
+            movies: [],
+            loading: true,
+            searchTerm
+        }
+        , () => {
+            const endPoint = !searchTerm ?
+                `${API_URL}movie/popular?api_key=${API_KEY}&${LANGUAGE}&page=1` :
+                `${API_URL}search/movie?api_key=${API_KEY}&${LANGUAGE}&query=${searchTerm}`;
+            this.fetchItems(endPoint);
+        });
+
     componentDidMount() {
         this.setState({
             loading: true
         });
-        const endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        const endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&${LANGUAGE}&page=1`;
         this.fetchItems(endPoint);
     }
 
     fetchItems = endPoint => fetch(endPoint)
         .then(result => result.json())
-        .then(result => this.setState(prevState => ({
-            movies: [...prevState.movies, ...result.results]
-        })));
+        .then(result => this.setState(prevState => {
+            console.log(result);
+            return ({
+                movies: [...prevState.movies, ...result.results],
+                heroImage: prevState.heroImage || result.results[0],
+                loading: false,
+                currentPage: result.page,
+                totalPages: result.total_pages
+            });
+        }));
 
     render() {
+        const {heroImage} = this.state;
         return <div className='rmdb-home'>
-            <HeroImage/>
-            <SearchBar/>
+            {heroImage ?
+                <div>
+                    <HeroImage image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
+                               title={heroImage.original_title}
+                               text={heroImage.overview}/>
+                    <SearchBar callback={this.searchItems}/>
+                </div> : null}
             <FourColGrid/>
             <Spinner/>
             <LoadMoreBtn/>
--- react/movie-app-demo/src/components/searchBar/SearchBar.js
+++ react/movie-app-demo/src/components/searchBar/SearchBar.js
@@ -1,14 +1,35 @@
 import React, {Component} from 'react';
-import FontAwesome from 'react-fontawesome';
 import './SearchBar.css'
+import FontAwesome from 'react-fontawesome';
 
 class SearchBar extends Component {
     state = {
-
+        value: ''
     };
 
+    timeout = null;
+
     render() {
-        return <div>SearchBar</div>
+        const {value} = this.state;
+
+        return <div className='rmdb-searchbar'>
+            <div className='rmdb-searchbar-content'>
+                <FontAwesome className='rmdb-fa-search' name='search' size='2x'/>
+                <input type='text'
+                       className='rmdb-searchbar-input'
+                       placeholder='Search'
+                       onChange={e => this.setState({value: e.target.value},
+                           () => {
+                               clearTimeout(this.timeout);
+                               const {callback} = this.props;
+                               const {value} = this.state;
+
+                               this.timeout = setTimeout(() => callback(value), 500)
+                           })}
+                       value={value}
+                />
+            </div>
+        </div>
     }
 }
 export default SearchBar;
